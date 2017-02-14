02737db07d41bc13ecf5d322e69db31012f2ea9b
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2017-02-14 12:24:08 +0300

    Minor

--- DevTeam.IoC/ParamsStateProvider.cs
+++ DevTeam.IoC/ParamsStateProvider.cs
@@ -1,23 +1,18 @@
 ﻿namespace DevTeam.IoC
 {
+    using System;
     using System.Linq;
     using Contracts;
 
     internal class ParamsStateProvider: IStateProvider
     {
         private readonly object[] _state;
-        private readonly int _hashCode;
+        private readonly Lazy<int> _hashCode;
 
         public ParamsStateProvider(params object[] state)
         {
             _state = state;
-            _hashCode = _state.Aggregate(0, (code, key) =>
-            {
-                unchecked
-                {
-                    return (code*397) ^ key.GetHashCode();
-                }
-            });
+            _hashCode = new Lazy<int>(GetHashInternal);
         }
 
         public object GetState(IResolverContext resolverContext, IStateKey stateKey)
@@ -44,7 +39,18 @@
 
         public override int GetHashCode()
         {
-            return _hashCode;
+            return _hashCode.Value;
+        }
+
+        private int GetHashInternal()
+        {
+            return _state.Aggregate(0, (code, key) =>
+            {
+                unchecked
+                {
+                    return (code*397) ^ key.GetHashCode();
+                }
+            });
         }
     }
 }
