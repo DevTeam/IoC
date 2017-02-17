bd6e95a07dffaf45759281e7511aefca017e5fb4
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2017-02-17 14:39:49 +0300

    Add samples

---
+++ Samples/Contacts/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿// ReSharper disable ArrangeTypeModifiers
+namespace ConsoleApp
+{
+    using System;
+    using DevTeam.IoC;
+    using DevTeam.IoC.Contracts;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            using (var container = new Container()
+                .Register().Contract<IContact>().State<string>(0).Autowiring(typeof(Contact)).ToSelf())
+            {
+                var johnContact = container.Resolve().State<string>(0).Instance<IContact>("John");
+                Console.WriteLine(johnContact.Name);
+            }
+        }
+
+        public Program(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    interface IContact
+    {
+        string Name { get; }
+    }
+
+    class Contact : IContact
+    {
+        public Contact([State] string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
